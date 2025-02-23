32c17b5ec4ee2517f94774625f28512b5fe7f505
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-02-23 23:53:16 +0000

    refactor CardList

--- app/src/main/java/com/arc/tcg/ui/screens/cardslist/CardList.kt
+++ app/src/main/java/com/arc/tcg/ui/screens/cardslist/CardList.kt
@@ -7,25 +7,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.SubcomposeAsyncImage
 import com.arc.tcg.data.model.CardBrief
+import com.arc.tcg.ui.screens.CustomImage
 import com.arc.tcg.ui.screens.LoadingBar
-import com.arc.tcg.utils.getImage
 
 @Composable
 fun CardList(viewModel: CardListViewModel = hiltViewModel(), onItemClicked: (itemId: String) -> Unit) {
@@ -63,15 +59,7 @@ fun ItemCard(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ){
-            SubcomposeAsyncImage(
-                model = getImage(item.image),
-                contentDescription = item.name,
-                modifier = Modifier.size(250.dp),
-                loading = {
-                    CircularProgressIndicator(Modifier.size(25.dp))
-                },
-                contentScale = ContentScale.Fit
-            )
+            CustomImage(item.image, item.name, 250.dp)
             Text(
                 text = item.name,
                 textAlign = TextAlign.Center,
