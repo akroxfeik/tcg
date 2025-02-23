14dbe69abc344a2104090faa48e88040eeff5a5a
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-02-23 04:46:07 +0000

    remove edgeToEdge (personal preference)

--- app/src/main/java/com/arc/tcg/MainActivity.kt
+++ app/src/main/java/com/arc/tcg/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -18,7 +17,6 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TcgTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
