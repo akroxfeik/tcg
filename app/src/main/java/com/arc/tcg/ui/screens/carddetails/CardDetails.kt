8beaafb48ac5aaaae299d7fcf8e76f4748d0da1c
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-02-23 04:34:43 +0000

    improve CardDetails

--- app/src/main/java/com/arc/tcg/ui/screens/carddetails/CardDetails.kt
+++ app/src/main/java/com/arc/tcg/ui/screens/carddetails/CardDetails.kt
@@ -1,5 +1,6 @@
 package com.arc.tcg.ui.screens.carddetails
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -17,6 +19,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.SubcomposeAsyncImage
 import com.arc.tcg.data.model.TCGCardDetail
+import com.arc.tcg.utils.ImageQuality
 import com.arc.tcg.utils.getImage
 
 @Composable
@@ -31,8 +34,8 @@ fun CardDetails(item: TCGCardDetail?) {
     item?.let {
         Column(
             Modifier
-                .padding(10.dp)
                 .fillMaxSize()
+                .padding(10.dp)
                 .verticalScroll(rememberScrollState())
         ) {
             Text(
@@ -40,14 +43,19 @@ fun CardDetails(item: TCGCardDetail?) {
                 fontSize = 20.sp,
                 modifier = Modifier.padding(5.dp)
             )
-            Column(Modifier.fillMaxSize()) {
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 SubcomposeAsyncImage(
-                    model = getImage(item.image),
+                    model = getImage(item.image, ImageQuality.HIGH),
                     contentDescription = item.name,
+                    modifier = Modifier.size(400.dp),
                     loading = {
                         CircularProgressIndicator(Modifier.size(25.dp))
                     },
-                    contentScale = ContentScale.Crop,
+                    contentScale = ContentScale.Fit,
                 )
             }
         }
