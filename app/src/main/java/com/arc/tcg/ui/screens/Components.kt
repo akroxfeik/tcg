bd3e2c15b807a3589b3f40f6458f2d03659028da
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-02-23 23:52:53 +0000

    refactor how to load image and make it reusable

--- app/src/main/java/com/arc/tcg/ui/screens/Components.kt
+++ app/src/main/java/com/arc/tcg/ui/screens/Components.kt
@@ -2,10 +2,21 @@ package com.arc.tcg.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.SubcomposeAsyncImage
+import com.arc.tcg.R
+import com.arc.tcg.data.model.CardBriefDetails
+import com.arc.tcg.utils.getImage
 
 @Composable
 fun LoadingBar() {
@@ -15,4 +26,37 @@ fun LoadingBar() {
     ) {
         CircularProgressIndicator()
     }
+}
+
+@Composable
+fun CustomImage(url: String?, name: String, size: Dp) {
+    SubcomposeAsyncImage(
+        model = getImage(url),
+        contentDescription = name,
+        modifier = Modifier.size(size),
+        loading = {
+            CircularProgressIndicator(Modifier.size(25.dp))
+        },
+        error = {
+            coil.compose.AsyncImage(
+                model = R.mipmap.cover,
+                contentDescription = name,
+                modifier = Modifier.size(250.dp),
+                contentScale = ContentScale.Fit
+            )
+        },
+        contentScale = ContentScale.Fit,
+        onError = {  }
+    )
+}
+
+@Composable
+fun TextProperty(value: String, id: Int) {
+    Text(
+        text = stringResource(id),
+        fontWeight = FontWeight.Bold
+    )
+    Text(
+        text = value
+    )
 }
