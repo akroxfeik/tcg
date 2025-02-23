579fd2cc9e88d87fd96cabbe359cd4e90749d582
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-02-23 04:03:20 +0000

    adds navigation to MainActivity

--- app/src/main/java/com/arc/tcg/MainActivity.kt
+++ app/src/main/java/com/arc/tcg/MainActivity.kt
@@ -6,13 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.arc.tcg.ui.screens.cardslist.CardList
+import androidx.navigation.compose.rememberNavController
+import com.arc.tcg.ui.navigation.AppNavHost
 import com.arc.tcg.ui.theme.TcgTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,25 +22,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             TcgTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
-                    CardList()
+                    AppNavHost(
+                        navController = rememberNavController()
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TcgTheme {
-        Greeting("Android")
     }
 }
