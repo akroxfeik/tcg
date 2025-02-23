99d6b86ff618c5dd31ad58281c39a5dc8c184ece
fernando-carvalho-91 <114769018+fernando-carvalho-91@users.noreply.github.com>
2025-02-23 04:04:25 +0000

    fix CardDetailsViewModel

--- app/src/main/java/com/arc/tcg/ui/screens/carddetails/CardDetailsViewModel.kt
+++ app/src/main/java/com/arc/tcg/ui/screens/carddetails/CardDetailsViewModel.kt
@@ -3,16 +3,22 @@ package com.arc.tcg.ui.screens.carddetails
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.arc.tcg.data.repository.MainRepository
+import com.arc.tcg.ui.navigation.Arg
 import com.arc.tcg.utils.NetworkHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CardDetailsViewModel @Inject constructor (
+    stateHandle: SavedStateHandle,
     private val repo: MainRepository,
     private val networkHelper: NetworkHelper
-) {
+): ViewModel() {
     var state by mutableStateOf(
         CardDetailsContract.State(
             card = null,
@@ -21,11 +27,11 @@ class CardDetailsViewModel @Inject constructor (
     )
 
     init {
-        val itemId = stateHandle.get<String>(Arg.CARD_ID)
+        val itemId = stateHandle.get<String>(Arg.ID)
         viewModelScope.launch { getCard(itemId) }
     }
 
-    private fun getCard(id: String) {
+    private fun getCard(id: String?) {
         viewModelScope.launch {
             if(networkHelper.isNetworkConnected()) {
                 repo.getCard(id).let {
